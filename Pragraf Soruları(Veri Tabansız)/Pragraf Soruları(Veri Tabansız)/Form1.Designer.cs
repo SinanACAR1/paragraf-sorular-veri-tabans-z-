e8cc314e3639359c1a934ee15d5e42c07e813d32
Sinan ACAR <sinanacar14654@gmail.com>
2017-07-17 22:47:28 +0300

    Add files via upload

---
+++ Pragraf Soruları(Veri Tabansız)/Pragraf Soruları(Veri Tabansız)/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.lblsorununcevabı = new System.Windows.Forms.Label();
+            this.lblsizincevabınız = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Enabled = false;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBox1.Location = new System.Drawing.Point(184, 42);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(556, 134);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(15, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 65);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Başla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(209, 211);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(176, 67);
+            this.button2.TabIndex = 2;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(423, 211);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(176, 67);
+            this.button3.TabIndex = 3;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(209, 311);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(176, 67);
+            this.button4.TabIndex = 4;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button5.Location = new System.Drawing.Point(423, 311);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(176, 67);
+            this.button5.TabIndex = 5;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // lblsorununcevabı
+            // 
+            this.lblsorununcevabı.AutoSize = true;
+            this.lblsorununcevabı.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblsorununcevabı.Location = new System.Drawing.Point(93, 385);
+            this.lblsorununcevabı.Name = "lblsorununcevabı";
+            this.lblsorununcevabı.Size = new System.Drawing.Size(16, 17);
+            this.lblsorununcevabı.TabIndex = 8;
+            this.lblsorununcevabı.Text = "0";
+            this.lblsorununcevabı.Visible = false;
+            // 
+            // lblsizincevabınız
+            // 
+            this.lblsizincevabınız.AutoSize = true;
+            this.lblsizincevabınız.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.lblsizincevabınız.Location = new System.Drawing.Point(3, 402);
+            this.lblsizincevabınız.Name = "lblsizincevabınız";
+            this.lblsizincevabınız.Size = new System.Drawing.Size(13, 13);
+            this.lblsizincevabınız.TabIndex = 9;
+            this.lblsizincevabınız.Text = "0";
+            this.lblsizincevabınız.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(48, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 24);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Soru:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(100, 60);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 24);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "0";
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button6.Location = new System.Drawing.Point(629, 311);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(176, 67);
+            this.button6.TabIndex = 12;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(746, 42);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(235, 134);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Doğru Yanlış";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(157, 58);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(16, 17);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(144, 29);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 17);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Yanlış";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(55, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 17);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(43, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 17);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Doğru";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label7.Location = new System.Drawing.Point(3, 385);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(93, 17);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Doğru cevap:";
+            this.label7.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.ClientSize = new System.Drawing.Size(984, 416);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblsizincevabınız);
+            this.Controls.Add(this.lblsorununcevabı);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Tag = "";
+            this.Text = "Paragraf Soruları";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label lblsorununcevabı;
+        private System.Windows.Forms.Label lblsizincevabınız;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Pragraf Soruları(Veri Tabansız)/Pragraf Soruları(Veri Tabansız)/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int soruno = 0;
+        int doğru = 0;
+        int yanlış = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+            button6.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = true;
+            button6.Enabled = true;
+            button1.Text = "İLERİ";
+            soruno++;
+            label4.Text = soruno.ToString();
+            lblsorununcevabı.Visible = false;
+            label7.Visible = false;
+
+            if (soruno == 1)
+            {
+                richTextBox1.Text = "1. Edebiyat; hava gibi, su gibi, güneş gibi, toprak gibi vazgeçilmezdi. Onunla yatılıp onunla kalkılıyordu ve yaratıcı gücünün sonsuzluğuna, edebiyatın insanı insan yapma büyüsüne İnanılıyordu. Toplumun yozlaşmaya ve her şeyin parayla ölçüldüğü, bilgi ve kültüre duyulan saygının, kredi kartlarına, görselliğe yönelmeye başladığı yıllarda, edebiyat 'Bir işlevi yok.' düşüncesiyle gazetelerden kovuldu. Edebiyat kovulunca da gazeteler çirkinleşti, gazetelerle birlikte dil de espri de düşünce de sığlaşıp yüceliğini yitirdi.";
+                button2.Text = "Ticari bir getirişi olmadığı gerekçesiyle gazetelerden uzaklaştırıldığına";
+                button3.Text = " Bir zamanlar yaşamın her alanında varlığını hissettirdiğine";
+                button4.Text = "Kendini yenileyemediği İçin ilgi görmediğine";
+                button5.Text = " İkinci plana itildiği için, dil, düşünce ve espri kalitesinin düştüğüne";
+                button6.Text = "Ticari zihniyetin egemen olmaya başla­masıyla etkisini yitirdiğine";
+                lblsorununcevabı.Text = "Kendini yenileyemediği İçin ilgi görmediğine";
+            }
+            if (soruno == 2)
+            {
+                richTextBox1.Text = "Türkçe benim biricik vatanım, memleketim, evim. Nereye gitsem, onu da beraberimde götürürüm. Ben Türkçeye tutkunum. Uzun yıllar Fransızca öğretmenliği yaptım. Fransızcayı da çok rahat konuşurum. Biraz İngilizce okudum, derken dört yıl İtalyanca kurslarına gittim, İtalyanca öğrendim. Bir parçacık da Almancaya el attım. Ama benim için Türkçenin yeri başka. Her insan kendi dilini sever; ama bence Türkçe dünya dillerinin en güzellerinden biri.";
+                button2.Text = " Yabancı dil öğrenmeye yatkın olma";
+                button3.Text = "Bir zamanlar yaşamın her alanında varlığını hissettirdiğine";
+                button4.Text = "Kendini yenileyemediği İçin ilgi görmediğine";
+                button5.Text = "İkinci plana itildiği için, dil, düşünce ve espri kalitesinin düştüğüne";
+                button6.Text = "Ticari zihniyetin egemen olmaya başla­masıyla etkisini yitirdiğine";
+                lblsorununcevabı.Text = "Ticari zihniyetin egemen olmaya başla­masıyla etkisini yitirdiğine";
+            }
+            if (soruno == 3)
+            {
+                richTextBox1.Text = "Gülhane Hayvanat Bahçesi'ne herkes gibi ilk olarak ailece gitmiştik. Zürafayı ve maymunları çok iyi hatırlıyorum. Alay Köşkü'nü uzun uzun incelemiş ve sonra parkın serinliğine kendimi bırakmıştım. Ama bu geziden bir türlü mutlu olamamıştım. Pislik içindeki küçük kafeslerinde sinir hastası olmuş aslanlar, maymunlar, domuzlar ve kediler görmekten içim daralmıştı. Her yer dışkı kokuyor ve her yer kaybedilen özgürlüğün ağıtını söylüyordu.Bu parçada dile getirilmek istenen duygu ya da davranış aşağıdakilerden hangisidir?";
+                button2.Text = "Pişmanlık";
+                button3.Text = "Şaşırma";
+                button4.Text = "Düş kırıklığına uğrama";
+                button5.Text = "Umursamama";
+                button6.Text = "Utanç duyma";
+                lblsorununcevabı.Text = "Düş kırıklığına uğrama";
+            }
+            if (soruno == 4)
+            {
+                richTextBox1.Text = " Resimde ele alınan konu çok çeşitli olabilir. (II) Sanatçı doğayı, sosyal, dini ya da ideolojik bir temayı, bilineni ya da bilinmeyeni ele alıp tuvaline taşıyabilir. (Ill) Zaten böyle bir özgürlüğe sahip değilse üretmiş olduğu eserin, sanat değeri taşıyıp taşımadığı tartışılır. (IV) Önemli olan sanatçının eserinde kendini anlatabilmesi, izleyenin eserde sanatçıyı görebilmesidir. (V) Bu nedenle konu, resmin temel öğelerinden biri değildir; sadece sanatçının iç dünyasını yüzeye aktarabilmesinde bir aracıdır.Bu parçada anlatılmak isteneni içeren en genel yargı numaralanmış cümlelerden hangisidir?";
+                button2.Text = "I.";
+                button3.Text = "II.";
+                button4.Text = "III.";
+                button5.Text = "IV";
+                button6.Text = "V";
+                lblsorununcevabı.Text = "V";
+            }
+            if (soruno == 5)
+            {
+                richTextBox1.Text = "Benim hiçbir öykümde önceden belirlenmiş bir iskelet olmadı. Roman üzerinde istediğim zaman çalışabilmeme karşın, öykünün "+"eşref saati"+"ni beklemem gerekirdi hep. Masanın başına oturup "+"Biraz öykü çalışayım."+" diyemedim hiç. Buna kalkıştığımda ise altını imzalamayacağım çok kötü şeyler yazdığımı gördüm. ........Bu parçanın sonuna, düşüncenin akışına göre aşağıdakilerden hangisi getirilebilir?";
+                button2.Text = " Roman, öykü gibi yazma süreci içinde oluşmuyor.";
+                button3.Text = "Roman, yazarın duygu dünyasını hiçbir zaman öykü kadar yansıtamaz";
+                button4.Text = "Öykünün iletisi, romanınki gibi baştan belli değildir";
+                button5.Text = "Öyküyle romanın yazılma sürecindeki asıl fark bu bence.";
+                button6.Text = "Öyküde olay ve kişi sayısı sınırlıdır, romana göre daha azdır.";
+                lblsorununcevabı.Text = "Öyküyle romanın yazılma sürecindeki asıl fark bu bence.";
+            }
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            lblsizincevabınız.Text = button5.Text;
+            if (lblsizincevabınız.Text == lblsorununcevabı.Text)
+            {
+                MessageBox.Show("Doğru cevap");
+                doğru++;
+                label2.Text = doğru.ToString();
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+                button5.Enabled = false;
+                button6.Enabled = false;
+            }
+            else
+            {
+                yanlış++;
+                label6.Text = yanlış.ToString();
+                label7.Visible = true;
+                lblsorununcevabı.Visible = true;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+                button5.Enabled = false;
+                button6.Enabled = false;
+            }
+          
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            lblsizincevabınız.Text = button4.Text;
+            if (lblsizincevabınız.Text == lblsorununcevabı.Text)
+            {
+                MessageBox.Show("Doğru cevap");
+                doğru++;
+                label2.Text = doğru.ToString();
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+                button5.Enabled = false;
+                button6.Enabled = false;
+            }
+            else
+            {
+                yanlış++;
+                label6.Text = yanlış.ToString();
+                label7.Visible = true;
+                lblsorununcevabı.Visible = true;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+                button5.Enabled = false;
+                button6.Enabled = false;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            lblsizincevabınız.Text = button3.Text;
+            if (lblsizincevabınız.Text == lblsorununcevabı.Text)
+            {
+                MessageBox.Show("Doğru cevap");
+                doğru++;
+                label2.Text = doğru.ToString();
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+                button5.Enabled = false;
+                button6.Enabled = false;
+            }
+            else
+            {
+                yanlış++;
+                label6.Text = yanlış.ToString();
+                label7.Visible = true;
+                lblsorununcevabı.Visible = true;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+                button5.Enabled = false;
+                button6.Enabled = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lblsizincevabınız.Text = button2.Text;
+            if (lblsizincevabınız.Text == lblsorununcevabı.Text)
+            {
+                MessageBox.Show("Doğru cevap");
+                doğru++;
+                label2.Text = doğru.ToString();
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+                button5.Enabled = false;
+                button6.Enabled = false;
+            }
+            else
+            {
+                yanlış++;
+                label6.Text = yanlış.ToString();
+                label7.Visible = true;
+                lblsorununcevabı.Visible = true;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+                button5.Enabled = false;
+                button6.Enabled = false;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            lblsizincevabınız.Text = button6.Text;
+            if (lblsizincevabınız.Text == lblsorununcevabı.Text)
+            {
+                MessageBox.Show("Doğru cevap");
+                doğru++;
+                label2.Text = doğru.ToString();
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+                button5.Enabled = false;
+                button6.Enabled = false;
+            }
+            else
+            {
+                yanlış++;
+                label6.Text = yanlış.ToString();
+                label7.Visible = true;
+                lblsorununcevabı.Visible = true;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+                button5.Enabled = false;
+                button6.Enabled = false;
+            }
+        }
+    }
+}
